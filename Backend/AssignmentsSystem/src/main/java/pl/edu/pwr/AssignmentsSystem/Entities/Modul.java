706bb5e5c53ddf51052568f241d74bd95c25cf39
Paulina Wątroba <paulina@watroba.com.pl>
2020-01-23 13:44:24 +0100

    Modul  - Entity

---
+++ Backend/AssignmentsSystem/src/main/java/pl/edu/pwr/AssignmentsSystem/Entities/Modul.java
@@ -0,0 +1,63 @@
+package pl.edu.pwr.AssignmentsSystem.Entities;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Modul {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private int id;
+    private int liczbaGodzin;
+    private String nazwa;
+    private int ogolnouczelniany;
+    private int wybieralny;
+
+    public Modul() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLiczbaGodzin() {
+        return liczbaGodzin;
+    }
+
+    public void setLiczbaGodzin(int liczbaGodzin) {
+        this.liczbaGodzin = liczbaGodzin;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public int getOgolnouczelniany() {
+        return ogolnouczelniany;
+    }
+
+    public void setOgolnouczelniany(int ogolnouczelniany) {
+        this.ogolnouczelniany = ogolnouczelniany;
+    }
+
+    public int getWybieralny() {
+        return wybieralny;
+    }
+
+    public void setWybieralny(int wybieralny) {
+        this.wybieralny = wybieralny;
+    }
+}
