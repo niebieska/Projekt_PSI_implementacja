d66ba9254bce57fe64f1af99d3d9c7e373085abb
Paulina Wątroba <paulina@watroba.com.pl>
2020-01-23 11:12:50 +0100

    Backend + miejsce na Frontend

---
+++ Backend/AssignmentsSystem/src/main/java/pl/edu/pwr/AssignmentsSystem/AssignmentsSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.AssignmentsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentsSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/AssignmentsSystem/src/test/java/pl/edu/pwr/AssignmentsSystem/AssignmentsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.AssignmentsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
