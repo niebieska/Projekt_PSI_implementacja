55cc36b269c2438cb767ac6d19ecaa12edb0716a
Mykhailo25 <248432@student.pwr.edu.pl>
2020-01-23 15:31:22 +0100

    dodanie jeszcze 2 pracownikow

--- SystemKadrowy/HRSystem/src/main/java/pl/edu/pwr/HRSystem/HrSystemApplication.java
+++ SystemKadrowy/HRSystem/src/main/java/pl/edu/pwr/HRSystem/HrSystemApplication.java
@@ -13,6 +13,14 @@ public class HrSystemApplication {
 		Pracownik pracownik = new Pracownik("Paulina", "Wątroba",
 				"paulina.watroba@pwr.edu.pl","Paulina.Wantroba", "Inż","Pracownik  zewnątrzny",
 				"Pracownik  zewnątrzny", 240, true, true);
+
+		Pracownik pracownik1 = new Pracownik("Mykhailo", "Stavniichuk",
+				"mykhailo.stavniichuk@pwr.edu.pl","Mykhailo.Stavniichuk", "Inż","Pracownik  zewnątrzny",
+				"Pracownik  zewnątrzny", 240, true, true);
+
+		Pracownik pracownik2 = new Pracownik("Mateusz", "Klimiuk",
+				"mateusz.klimiuk@pwr.edu.pl","Mateusz.Klimiuk", "Inż","Pracownik  zewnątrzny",
+				"Pracownik  zewnątrzny", 240, true, true);
 	}
 
 }
