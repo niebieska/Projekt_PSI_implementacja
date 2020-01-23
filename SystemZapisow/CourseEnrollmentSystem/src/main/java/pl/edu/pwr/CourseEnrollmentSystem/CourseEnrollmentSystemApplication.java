a7ff080b056554152f4ba6652cfe72412e32e4a5
Paulina Wątroba <paulina@watroba.com.pl>
2020-01-23 11:25:24 +0100

    System Zapisów

---
+++ SystemZapisow/CourseEnrollmentSystem/src/main/java/pl/edu/pwr/CourseEnrollmentSystem/CourseEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.CourseEnrollmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrollmentSystemApplication.class, args);
+	}
+
+}
