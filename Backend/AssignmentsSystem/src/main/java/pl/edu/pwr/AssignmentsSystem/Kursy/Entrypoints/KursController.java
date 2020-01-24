aac453d91e0d45980c66571655cbfbb9231e7d69
Mateusz Klimiuk <mateuszwodan@gmail.com>
2020-01-24 15:35:08 +0100

    Add service

--- Backend/AssignmentsSystem/src/main/java/pl/edu/pwr/AssignmentsSystem/Kursy/Entrypoints/KursController.java
+++ Backend/AssignmentsSystem/src/main/java/pl/edu/pwr/AssignmentsSystem/Kursy/Entrypoints/KursController.java
@@ -1,17 +1,24 @@
 package pl.edu.pwr.AssignmentsSystem.Kursy.Entrypoints;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pwr.AssignmentsSystem.Kursy.Entrypoints.Dto.IdentyfikatorSemestruDto;
 import pl.edu.pwr.AssignmentsSystem.Kursy.Entrypoints.Dto.PlanStudiowDto;
+import pl.edu.pwr.AssignmentsSystem.Kursy.Usecase.PlanStudiowService;
 
 import java.util.List;
 
 @RestController
 public class KursController {
 
+    @Autowired
+    private PlanStudiowService planStudiowService;
+
     @GetMapping("/getAllKurs")
-    public List<PlanStudiowDto> getAllKursy()
-    {
-        return null;
+    public PlanStudiowDto getAllKursy(@RequestParam IdentyfikatorSemestruDto identyfikatorSemestruDto) {
+        return planStudiowService
+                .getPlanStudiowForKey(identyfikatorSemestruDto);
     }
 }
