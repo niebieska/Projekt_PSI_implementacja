836da489cd3d37a0eb55c8a2866fd72938b3c96a
Mykhailo25 <248432@student.pwr.edu.pl>
2020-02-02 12:29:59 +0100

    dodanie skryptow

---
+++ Kubernetes_p/Start-kubernetes.sh
@@ -0,0 +1,37 @@
+#memcache
+kubectl apply -f /c/Users/Kubernetes_p/Memcache/memcache-deployment.yaml
+
+#db
+kubectl apply -f /c/Users/Kubernetes_p/db-deployment.yaml
+
+#hrsystem
+kubectl apply -f /c/Users/Kubernetes_p/hrsystem-deployment.yaml
+
+#cesystem
+kubectl apply -f /c/Users/Kubernetes_p/cesystem-deployment.yaml
+
+#ausystem
+kubectl apply -f /c/Users/Kubernetes_p/ausystem-deployment.yaml
+
+#frontend
+kubectl apply -f /c/Users/Kubernetes_p/frontend-deployment.yaml
+
+#assystem
+kubectl apply -f /c/Users/Kubernetes_p/assystem-deployment.yaml
+sleep 60s
+
+
+#memcache
+kubectl apply -f /c/Users/Kubernetes_p/Memcache/memcache-service.yaml
+#db
+kubectl apply -f /c/Users/Kubernetes_p/db-service.yaml
+#hrsystem
+kubectl apply -f /c/Users/Kubernetes_p/hrsystem-service.yaml
+#ausystem
+kubectl apply -f /c/Users/Kubernetes_p/ausystem-service.yaml
+#cesystem
+kubectl apply -f /c/Users/Kubernetes_p/cesystem-service.yaml
+#frontend
+kubectl apply -f /c/Users/Kubernetes_p/frontend-service.yaml
+#assystem
+kubectl apply -f /c/Users/Kubernetes_p/assystem-service.yaml
